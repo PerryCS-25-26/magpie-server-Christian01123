5abda320e0a9d53294a904917b33143f214a0196
Christian <christianandersen2007@gmail.com>
2025-12-02 09:45:54 -0700

    Fixed indents

--- MagpieServer.java
+++ MagpieServer.java
@@ -8,39 +8,39 @@ import java.net.HttpURLConnection;
 import java.util.Date;
 
 public class MagpieServer implements HttpHandler {
-    public void handle(HttpExchange exchange) throws IOException {
-        final String requestMethod = exchange.getRequestMethod();
-        String requestPath = exchange.getRequestURI().getPath();
+public void handle(HttpExchange exchange) throws IOException {
+    final String requestMethod = exchange.getRequestMethod();
+    String requestPath = exchange.getRequestURI().getPath();
 
-        // Log the request
-        System.out.println(new Date() + " " + requestMethod + " request for: " + requestPath);
+    // Log the request
+    System.out.println(new Date() + " " + requestMethod + " request for: " + requestPath);
 
-        // Attempt to serve a file from the public folder for any GET request
-        if ("GET".equals(requestMethod)) {
+    // Attempt to serve a file from the public folder for any GET request
+    if ("GET".equals(requestMethod)) {
 
-        }
-        else {
-            // If the request is not a GET request, return a 405 Method Not Allowed error
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_METHOD, 0);
-            exchange.getResponseBody().close();
-        }
+    }
+    else {
+        // If the request is not a GET request, return a 405 Method Not Allowed error
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_METHOD, 0);
+        exchange.getResponseBody().close();
+    }
+}
+
+public static void main (String[] args) {
+    // Set the default port to 8080 and the root folder to the public folder
+    int port = 8080;
+    String rootFolder = "./public";
+
+    // Create and start a new FileServer to handle file requests
+    try {
+        final HttpServer server = buildFileServer(port, rootFolder);
+        server.start();
+    }
+    catch (IOException e) {
+        System.err.println(new Date() + " Error starting server: " + e);
+        System.exit(1); 
     }
 
-    public static void main (String[] args) {
-        // Set the default port to 8080 and the root folder to the public folder
-        int port = 8080;
-        String rootFolder = "./public";
-
-        // Create and start a new FileServer to handle file requests
-        try {
-            final HttpServer server = buildFileServer(port, rootFolder);
-            server.start();
-        }
-        catch (IOException e) {
-            System.err.println(new Date() + " Error starting server: " + e);
-            System.exit(1); 
-        }
-
-        System.out.println(new Date() + " Server listening at http://localhost:" + port);
-    }
+    System.out.println(new Date() + " Server listening at http://localhost:" + port);
+}
 }
